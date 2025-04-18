825c18c5d2115cfc11aff14d361ed2e5df8b537b
212301 <212301@DESKTOP-4SFIC8R>
2025-04-18 10:54:18 +0300

    сделали зачёркивание элементов

--- ToDoList_Theory/MainWindow.xaml.cs
+++ ToDoList_Theory/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,24 +36,54 @@ namespace ToDoList_Theory
             {
                 Tasks.Add(new ToDoItem { Title = TaskInputTb.Text, IsDone = false });
                 TaskInputTb.Text = string.Empty; // string.Empty - очищает строку
+                UpdateCounter();
             }
         }
 
         private void DeleteTaskBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            if (sender is Button btn && btn.DataContext is ToDoItem item)
+            {
+                Tasks.Remove(item);
+                UpdateCounter();
+            }
         }
 
-        private void ChechBox_Changed (object sender, RoutedEventArgs e)
+        private void ChechBox_Changed(object sender, RoutedEventArgs e)
         {
+            UpdateCounter();
+        }
 
+        public void UpdateCounter()
+        {
+            int counter = 0;
+            foreach (var task in Tasks)
+            {
+                if (!task.IsDone)
+                {
+                    counter++;
+                }
+            }
+            CounterTextTbl.Text = $"Осталось дел: {counter}";
         }
 
         public class ToDoItem
         {
             public string Title { get; set; }
             public bool IsDone { get; set; }
-
         }
+        public class DoneToTextDecorationConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                bool isDone = (bool)value;
+                return isDone ? TextDecorations.Strikethrough : null;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                return null;
+            }
         }
     }
+}
