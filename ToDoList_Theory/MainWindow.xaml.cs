417a477cbde303b9ee26d7081d3d185db2b35d08
212301 <212301@1220-014-22>
2025-04-22 13:52:38 +0300

    Версия Софии Алексеевны, рабочая

--- ToDoList_Theory/MainWindow.xaml.cs
+++ ToDoList_Theory/MainWindow.xaml.cs
@@ -15,45 +15,44 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ToDoList_Theory
+
+namespace ToDoList221
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public ObservableCollection<ToDoItem> Tasks { get; set; } = new ObservableCollection<ToDoItem>();
+        public ObservableCollection<TodoItem> Tasks { get; set; } = new ObservableCollection<TodoItem>();
         public MainWindow()
         {
             InitializeComponent();
             TaskListLb.ItemsSource = Tasks;
+            UpdateCounter();
         }
 
         private void AddTaskBtn_Click(object sender, RoutedEventArgs e)
         {
-            // IsNullOrWhiteSpace() - проверяет, то что строка NULL или пустая
             if (!string.IsNullOrWhiteSpace(TaskInputTb.Text))
             {
-                Tasks.Add(new ToDoItem { Title = TaskInputTb.Text, IsDone = false });
-                TaskInputTb.Text = string.Empty; // string.Empty - очищает строку
+                Tasks.Add(new TodoItem { Title = TaskInputTb.Text, IsDone = false });
+                TaskInputTb.Text = string.Empty;
                 UpdateCounter();
             }
         }
 
         private void DeleteTaskBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (sender is Button btn && btn.DataContext is ToDoItem item)
+            if (sender is Button btn && btn.DataContext is TodoItem item)
             {
                 Tasks.Remove(item);
                 UpdateCounter();
             }
         }
-
-        private void ChechBox_Changed(object sender, RoutedEventArgs e)
+        private void CheckBox_Changed(object sender, RoutedEventArgs e)
         {
             UpdateCounter();
         }
-
         public void UpdateCounter()
         {
             int counter = 0;
@@ -66,24 +65,23 @@ namespace ToDoList_Theory
             }
             CounterTextTbl.Text = $"Осталось дел: {counter}";
         }
-
-        public class ToDoItem
+    }
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+    public class DoneToTextDecorationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            public string Title { get; set; }
-            public bool IsDone { get; set; }
+            bool isDone = (bool)value;
+            return isDone ? TextDecorations.Strikethrough : null;
         }
-        public class DoneToTextDecorationConverter : IValueConverter
-        {
-            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            {
-                bool isDone = (bool)value;
-                return isDone ? TextDecorations.Strikethrough : null;
-            }
 
-            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            {
-                return null;
-            }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
         }
     }
 }
